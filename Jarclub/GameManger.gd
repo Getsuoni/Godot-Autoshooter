e35c984d14f8659de9d367e7829d2858db1cc7ac
Angelo Yannello <119885216+Getsuoni@users.noreply.github.com>
2024-04-18 11:59:21 -0800

    Delete Jarclub directory

--- Jarclub/GameManger.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-# Score number for every enemy killed
-var score = 0
-
-
--- Jarclub/Kill count.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-#Every time an enmey is killed from the badguy script it adds +1 to this counter
-func _process(_delta)->void:
-	self.text = "KILLS: " +str(GameManger.score)
